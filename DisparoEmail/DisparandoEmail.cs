7fc30c783244ad6ee4e03211e62b58c4219148ae
diegolins10 <dede140126>
2022-12-20 21:10:38 -0300

    Add project files.

---
+++ DisparoEmail/DisparandoEmail.cs
@@ -0,0 +1,26 @@
+﻿using System.ServiceProcess;
+using System.Threading;
+
+namespace DisparoEmail
+{
+    public partial class DisparandoEmail : ServiceBase
+    {
+        public DisparandoEmail()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        public void VerificarEmailPendente()
+        {
+
+        }
+    }
+}
